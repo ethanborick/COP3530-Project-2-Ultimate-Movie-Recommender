48d1923400becaf13bd68f2d30ebd2bcbcba9e1a
matilde-gillia <gilamonster.omg@gmail.com>
2025-10-29 23:54:55 -0400

    movies handle quotation titles, graph implementation

--- main.cpp
+++ main.cpp
@@ -1,48 +1,76 @@
 #include <iostream>
 #include <string>
-//#include <fstream>
-//#include <sstream>
-//#include <map>
-//#include "movie.h"
+#include <fstream>
+#include <sstream>
+#include <map>
+#include "movie.h"
 
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    /*
-    ifstream file("../ml-latest-small/moviedata.csv");
+
+    ifstream file("ml-latest-small/moviedata.csv");
+    if (!file.is_open()) {  //this was for debugging, can probably delete now
+        cout << "Error: Could not open moviedata.csv" << endl;
+        return 1;
+    }
     string line;
     map<string, MovieNode*> rating_counts;
     while (getline(file, line)) {
         stringstream stream(line);
         string id, name, genres, rating;
         getline(stream, id, ',');
-        getline(stream, name, ',');
-        getline(stream, genres, ',');
-        getline(stream, rating, ',');
-        if (rating_counts.find(name) == rating_counts.end()) {
-            rating_counts[name] = new MovieNode(name, stoi(id), genres);
 
+        //handling quoted and unquoted movie names
+            //quoted movie names have commas inside them
+        if (stream.peek() == '"') {
+            stream.get();
+            getline(stream, name, '"');
+
+            size_t pos = 0;
+            while ((pos = name.find("\"\"", pos)) != string::npos) {
+                name.replace(pos, 2, "\"");
+                ++pos;
+            }
+
+            if (stream.peek() == ',') stream.get();
+        }
+        else {
+            getline(stream, name, ',');
+        }
+
+        getline(stream, genres, ',');
+        getline(stream, rating);
+
+        int movieId;
+        double ratingVal;
+        if (rating_counts.find(name) == rating_counts.end()) {
+            rating_counts[name] = new MovieNode(name, movieId, genres);
         }
         rating_counts[name]->rating_count++;
-        rating_counts[name]->total_rating += stod(rating);
+        rating_counts[name]->total_rating += ratingVal;
     }
+    
     for (auto it = rating_counts.begin(); it != rating_counts.end(); it++) {
         MovieNode* movie = it->second;
         movie->avg_rating = movie->total_rating / double(movie->rating_count);
     }
-    */
+    
+    MovieGraph graph;
+    graph.buildGraph(rating_counts);
+    graph.printGraph();
     // loop through the map and calculate each movies average rating
     // loop through the map and insert each movie node into the graph
 
-    string input = argv[1];
-    string traversal = argv[2];
-    int time = 1;
+    // string input = argv[1];
+    // string traversal = argv[2];
+    // int time = 1;
 
-    cout << "Time taken for " << traversal << ": " << time << endl;
-    for (int i = 1; i < 11; i++) {
-        cout << i << ".) " << input << endl;
-        cout << "   Genre: genre" << endl;
-        cout << "   Average Rating: 00%" << endl;
-    }
+    // cout << "Time taken for " << traversal << ": " << time << endl;
+    // for (int i = 1; i < 11; i++) {
+    //     cout << i << ".) " << input << endl;
+    //     cout << "   Genre: genre" << endl;
+    //     cout << "   Average Rating: 00%" << endl;
+    // }
     return 0;
 }
--- movie.h
+++ movie.h
@@ -3,6 +3,9 @@
 #include <fstream>
 #include <sstream>
 #include <map>
+#include <vector>
+#include <iterator>
+#include <ctime>
 using namespace std;
 
 struct MovieNode {
@@ -34,5 +37,64 @@ struct MovieNode {
 };
 
 class MovieGraph {
+    // //insertion function     FROM LECTURE SLIDES, influenced uncommented code below
+    // int no_lines;
+    // string from, to, wt;
+    // map<string, vector<pair<string,int>>> graph;
+    // map<string, vector<pair<string,int>>>::iterator it;
+    
+    // cin >> no_lines;
+    
+    // for(int i = 0; i < no_lines; i++)
+    // {
+    //     cin >> from >> to >> wt;
+        
+    //     //Creating adjacency list
+    //     graph[from].push_back(make_pair(to, stoi(wt)));
+        
+    //     if (graph.find(to)==graph.end())
+    //     {
+    //         graph[to] = {};
+    //     }
+    // }
+
+    public:
+    map<string, vector<pair<string, int>>> graph;
+
+    //undirected graph
+    void insertEdge(const string& from, const string& to, int weight) {
+        graph[from].push_back(make_pair(to, weight));
+        graph[to].push_back(make_pair(from, weight));
+    }
+
+    void buildGraph(const map<string, MovieNode*>& movies) {
+        vector<string> titles;
+        for (auto& pair : movies) {
+            titles.push_back(pair.first);
+        }
+
+        //random edges
+        srand(time(nullptr));
+        for (auto& from : titles) {
+            int connections = rand() % 3 + 1; //1–3 random connections, keep sparse since using adjacency list
+            for (int i = 0; i < connections; i++) {
+                string to = titles[rand() % titles.size()];
+                if (to != from)
+                    insertEdge(from, to, 1); //weight = 1 for all edges: unweighted graph
+            }
+        } 
+    }
+
+    void printGraph() {
+        int i = 0;
+        for (auto it = graph.begin(); it != graph.end(); ++it, ++i) {
+            cout << it->first << " -> ";
+            for (int j = 0; j < it->second.size(); j++) {
+                cout << "(" << it->second[j].first << ", " << it->second[j].second << ") ";
+            }
+            cout << endl;
+        }
+    }
+    // //search functions
 
 };

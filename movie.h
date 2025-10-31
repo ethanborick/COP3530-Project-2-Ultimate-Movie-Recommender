78949828035e3f3e3b156d95840a1e6e526da87d
ethanborick <eborick@ufl.edu>
2025-10-31 13:20:56 -0400

    added bfs

--- movie.h
+++ movie.h
@@ -59,9 +59,18 @@ class MovieGraph {
     //         graph[to] = {};
     //     }
     // }
-
-    public:
     map<string, vector<MovieNode*>> graph;
+    map<int, string> g = { // for printing genres to the front end
+            {0, "Action"}, {1, "Adventure"}, {2, "Animation"},
+            {3, "Children"}, {4, "Comedy"}, {5, "Crime"},
+            {6, "Documentary"}, {7, "Drama"}, {8, "Fantasy"},
+            {9, "Film-Noir"}, {10, "Horror"}, {11, "IMAX"},
+            {12, "Musical"}, {13, "Mystery"}, {14, "Romance"},
+            {15, "Sci-Fi"}, {16, "Thriller"}, {17, "War"},
+            {18, "Western"}
+    };
+    public:
+
 
     //undirected graph
     void insertEdge(MovieNode* from, MovieNode* to) {
